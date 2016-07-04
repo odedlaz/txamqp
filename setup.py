8c902dc0c7634cc0603b4c756ff3e9572df9d8c1
Oded Lazar <odedlaz@gmail.com>
2016-07-04 14:13:57 +0300

    Amend setup.py to indicate new maintainer

--- setup.py
+++ setup.py
@@ -1,30 +1,43 @@
-setupdict= {
-    'name': 'txAMQP',
-    'version': '0.6.2',
-    'author': 'Esteve Fernandez',
-    'author_email': 'esteve@fluidinfo.com',
-    'url': 'https://launchpad.net/txamqp',
-    'description': 'Python library for communicating with AMQP peers and brokers using Twisted',
-    'long_description': '''This project contains all the necessary code to connect, send and receive messages to/from an AMQP-compliant peer or broker (Qpid, OpenAMQ, RabbitMQ) using Twisted.
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-It also includes support for using Thrift RPC over AMQP in Twisted applications.
 
-txAMQP is sponsored by the friendly folks at Fluidinfo (http://www.fluidinfo.com).'''
-    }
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-    setupdict['packages'] = ['txamqp', 'txamqp.contrib', 'txamqp.contrib.thrift']
-    setupdict['package_dir'] = {
-        'txamqp': 'src/txamqp',
-        'txamqp.contrib': 'src/txamqp/contrib',
-        'txamqp.contrib.thrift': 'src/txamqp/contrib/thrift',
-    }
-else:
-    setupdict['packages'] = find_packages('src')
-    setupdict['package_dir'] = { '': 'src' }
-    setupdict['install_requires'] = ['Twisted']
-        
-setup(**setupdict)
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+long_description = '''This project contains all the necessary code to connect, send and receive messages to/from an AMQP-compliant peer or broker (Qpid, OpenAMQ, RabbitMQ) using Twisted.
+It also includes support for using Thrift RPC over AMQP in Twisted applications.'''
+
+setup_dict = dict(name='txamqp',
+                  version='0.6.2',
+                  url='http://github.com/odedlaz/txamqp',
+                  license='Apache License 2.0',
+                  author='Oded Lazar',
+                  tests_require=['pytest'],
+                  cmdclass={'test': PyTest},
+                  install_requires=['Twisted>=16.2.0'],
+                  author_email='odedlaz@gmail.com',
+                  description='Python library for communicating with AMQP peers and brokers using Twisted',
+                  long_description=long_description,
+                  packages=['txamqp', 'txamqp.contrib', 'txamqp.contrib.thrift'],
+                  package_dir={'txamqp': 'src/txamqp',
+                               'txamqp.contrib': 'src/txamqp/contrib',
+                               'txamqp.contrib.thrift': 'src/txamqp/contrib/thrift',
+                               },
+                  platforms='any',
+                  extras_require={
+                      'testing': ['pytest'],
+                  })
+
+setup(**setup_dict)
